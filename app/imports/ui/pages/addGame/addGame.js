60c256ea8f4f0d16a3f261c9b372e3f578e74d3d
kodayv <kodayv@hawaii.edu>
2017-04-04 12:46:20 -1000

    working on addgame page2

--- app/imports/ui/pages/addGame/addGame.js
+++ app/imports/ui/pages/addGame/addGame.js
@@ -4,19 +4,43 @@ import { _ } from 'meteor/underscore';
 import { GameTemplate, GameSchema } from '../../../api/template/template.js';
 import { FlowRouter } from 'meteor/kadira:flow-router';
 import { Meteor } from 'meteor/meteor';
+import { Games } from '/imports/api/games/GameCollection';
+import { Categories } from '/imports/api/categories/CategoryCollection';
 
-// import { username } from '/app/imports/startup/client/useraccount-configuration.js';
-
-
-/* eslint-disable no-param-reassign */
-
+const displaySuccessMessage = 'displaySuccessMessage';
 const displayErrorMessages = 'displayErrorMessages';
+
+export const gameObjects = [{ label: 'Chess', value: '1' },
+  { label: 'Monopoly', value: '2' },
+  { label: 'Scrabble', value: '3' },
+  { label: 'Backgammon', value: '4' },
+  { label: 'Other', value: '5' }];
+
+export const maxPlayerObjects = [{ label: '2 players', value: '2' },
+  { label: '3 players', value: '3' },
+  { label: '4 players', value: '4' },
+  { label: '5 players', value: '5' },
+  { label: '6 players', value: '6' },
+  { label: '7 players', value: '7' },
+  { label: '8 players', value: '8' },
+  { label: '9 players', value: '9' },
+  { label: '10+ players', value: '10' }];
+
+export const lengthObjects = [{ label: '1 hour', value: '1' },
+  { label: '2 hours', value: '2' },
+  { label: '3 hours', value: '3' },
+  { label: '4+ hours', value: '4' }];
+
+export const smokingList = ['Allowed'];
+export const reoccurringList = ['Reoccurring'];
 
 
 Template.AddGame_Page.onCreated(function onCreated() {
+  this.subscribe(Categories.getPublicationName());
+  this.subscribe(Games.getPublicationName());
   this.messageFlags = new ReactiveDict();
   this.messageFlags.set(displayErrorMessages, false);
-  this.context = GameSchema.namedContext('Add_Contact_Page');
+  this.context = GameSchema.namedContext('AddGame_Page');
 });
 
 Template.AddGame_Page.helpers({
@@ -27,6 +51,32 @@ Template.AddGame_Page.helpers({
     const invalidKeys = Template.instance().context.invalidKeys();
     const errorObject = _.find(invalidKeys, (keyObj) => keyObj.name === fieldName);
     return errorObject && Template.instance().context.keyErrorMessage(errorObject.name);
+  },
+  game() {
+    return Games.findDoc(FlowRouter.getParam('username'));
+  },
+  games() {
+    return gameObjects;
+  },
+  players() {
+    return maxPlayerObjects;
+  },
+  gameLength() {
+    return lengthObjects;
+  },
+  smoking() {
+    return _.map(smokingList, function makeSmokingObject(smoking) { return { label: smoking }; });
+  },
+  reoccurring() {
+    return _.map(reoccurringList, function makeReoccurringObject(reoccurring) { return { label: reoccurring }; });
+  },
+  categories() {
+    const game = Games.findDoc(FlowRouter.getParam('username'));
+    const selectedCategories = game.categories;
+    return game && _.map(Categories.findAll(),
+            function makeInterestObject(category) {
+              return { label: category.name, selected: _.contains(selectedCategories, category.name) };
+            });
   },
 });
 
