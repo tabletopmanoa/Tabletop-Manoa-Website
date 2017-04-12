889526ae7ab80492e76969ecee66485567bf8ebc
Jory Leech <joryleech@gmail.com>
2017-04-11 21:54:07 -1000

    Linked browse to database
    Todo: search function
    needed to change workload

--- app/imports/ui/pages/games/games-page.js
+++ app/imports/ui/pages/games/games-page.js
@@ -1,52 +1,33 @@
 import { Template } from 'meteor/templating';
-import { ReactiveDict } from 'meteor/reactive-dict';
-import { _ } from 'meteor/underscore';
-import { Games } from '/imports/api/games/GameCollection';
-import { Categories } from '/imports/api/categories/CategoryCollection';
-import { Listings } from '/imports/api/listings/ListCollection';
+import {GameTemplate} from '../../../api/template/template.js'
+import {ReactiveDict } from 'meteor/reactive-dict';
 
-
-const selectedCategoriesKey = 'selectedCategories';
-
-export const categoryList = ['Role Playing', 'Card Games', 'Board Games', 'Miniatures'];
-
-Template.Games_Page.onCreated(function onCreated() {
-  this.subscribe(Games.getPublicationName());
-  this.subscribe(Categories.getPublicationName());
-  this.messageFlags = new ReactiveDict();
-  this.messageFlags.set(selectedCategoriesKey, undefined);
-});
+Template.Games_Page.onCreated(
+  function bodyOnCreated(){
+    this.state=new ReactiveDict();
+  }
+);
 
 Template.Games_Page.helpers({
-  games() {
-    // Initialize selectedInterests to all of them if messageFlags is undefined.
-    if (!Template.instance().messageFlags.get(selectedCategoriesKey)) {
-      Template.instance().messageFlags.set(selectedCategoriesKey, _.map(Categories.findAll(), category => category.name));
+  gamesList(){
+    return GameTemplate.find();
+  },
+  message(){
+    const instance = Template.instance();
+    if(instance.state.get('magic-checked')){
+      return "true";
+    }else{
+      return "false";
     }
-    // Find all games with the currently selected interests.
-    const allGames = Games.findAll();
-    const selectedCategories = Template.instance().messageFlags.get(selectedCategoriesKey);
-    return _.filter(allGames, games => _.intersection(games.category, selectedCategories).length > 0);
-  },
-
-  categories() {
-    return _.map(categoryList,
-        function setCategory (category) {
-          return {
-            label: category };
-        });
-  },
+  }
 });
 
+
 Template.Games_Page.events({
-  'submit .filter-data-form'(event, instance) {
-    event.preventDefault();
-    const selectedCategories = [];
-    _.each(categoryList, function setCategories (category){
-      if (event.target[category].checked) {
-        selectedCategories.push(event.target[category].value);
-      }
-    });
-    instance.messageFlags.set(selectedCategoriesKey, _.map(selectedCategories, (option) => option.value));
-  },
+  'change magic-checkbox'(event, instance){
+
+    instance.state.set('magic-checked',event.target.checked);
+  }
+
+
 });
