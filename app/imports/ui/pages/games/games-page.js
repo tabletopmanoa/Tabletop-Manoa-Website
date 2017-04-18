8f8e2d246cccb1895e510b3e29c8d576c6689d99
Jory Leech <joryleech@gmail.com>
2017-04-18 10:14:42 -1000

    made category

--- app/imports/ui/pages/games/games-page.js
+++ app/imports/ui/pages/games/games-page.js
@@ -11,18 +11,20 @@ Template.Games_Page.onCreated(
 
 Template.Games_Page.helpers({
   gamesList(){
-    if(instance.state.get('magic-checked')) {
-
+   const instance=Template.instance();
+    if(instance.state.get('category').equals('all')){
+      console.log("works here");
+      return GameTemplate.find();
     }
+    else{
 
+      return GameTemplate.find({},{fields:{category:instance.state.get('category')}});
+    }
   },
+
   message(){
     const instance = Template.instance();
-    if(instance.state.get('magic-checked')){
-      return "true";
-    }else{
-      return "false";
-    }
+    return instance.state.get('category');
   }
 });
 
@@ -31,7 +33,27 @@ Template.Games_Page.events({
   'change #magic-checkbox'(event, instance){
     console.log("magic");
     instance.state.set('magic-checked',event.target.checked);
-  }
-
+  },
+  'change #mini-games'(event,instance){
+    instance.state.set('mini-games',event.target.checked);
+    instance.state.set('category','mini');
+  },
+  'change #card-games'(event,instance){
+    instance.state.set('card-games',event.target.checked);
+    instance.state.set('category','card');
+  },
+  'change #rp-games'(event,instance){
+    instance.state.set('rp-games',event.target.checked);
+    instance.state.set('category','role');
+  },
+  'change #all-games'(event,instance){
+    instance.state.set('all-games',event.target.checked);
+    instance.state.set('category','all');
+  },
+  'change #board-games'(event,instance){
+    instance.state.set('board-games',event.target.checked);
+    instance.state.set('category','board');
+    console.log(event.target.checked);
+  },
 
 });
