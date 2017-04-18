948118dd8f0d18ece9188ce972219a0a4e8563db
kodayv <kodayv@hawaii.edu>
2017-04-18 09:34:09 -1000

    completed testing

--- app/imports/api/games/GameCollection.test.js
+++ app/imports/api/games/GameCollection.test.js
@@ -1,7 +1,7 @@
 /* eslint prefer-arrow-callback: "off", no-unused-expressions: "off" */
 /* eslint-env mocha */
 
-// import { Games } from 'GameCollection.js';
+import { Games } from './GameCollection.js';
 import { Categories } from '/imports/api/categories/CategoryCollection';
 import { Meteor } from 'meteor/meteor';
 import { expect } from 'chai';
@@ -14,18 +14,17 @@ if (Meteor.isServer) {
     const gameName = 'Pathfinder';
     const category = [categoryName];
     const maxPlayers = 10;
+    const date = new Date("April 29, 2017 11:13:00");
     const gameLength = '4 hours';
     const location = 'Hale Wina Lounge';
     const about = 'This game is very cool';
     const picture = 'http://www.levelupgamesmn.com/uploads/2/4/7/7/24777638/2796519_orig.png';
     const contact = 'kodayv@hawaii.edu';
     const resources = 'http://www.d20pfsrd.com/';
-    const defineObject = { gameName, category, maxPlayers, gameLength, location, about, picture, contact, resources };
+    const defineObject = { gameName, category, maxPlayers, date, gameLength, location, about, picture, contact, resources };
 
     before(function setup() {
       removeAllEntities();
-      // Define a sample interest.
-      Categories.define({ name: categoryName });
     });
 
     after(function teardown() {
@@ -41,13 +40,12 @@ if (Meteor.isServer) {
       expect(doc.maxPlayers).to.equal(maxPlayers);
       expect(doc.gameLength).to.equal(gameLength);
       expect(doc.location).to.equal(location);
+      expect(doc.date.getTime()).to.equal(date.getTime());
       expect(doc.category[0]).to.equal(categoryName);
       expect(doc.about).to.equal(about);
       expect(doc.picture).to.equal(picture);
       expect(doc.contact).to.equal(contact);
       expect(doc.resources).to.equal(resources);
-      // Check that multiple definitions with the same email address fail
-      expect(function foo() { Games.define(defineObject); }).to.throw(Error);
       // Check that we can dump and restore a Profile.
       const dumpObject = Games.dumpOne(docID);
       Games.removeIt(docID);
