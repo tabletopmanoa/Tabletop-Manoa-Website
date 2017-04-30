3272fa162c03ef274f43e00a3f79706d4c7a0c97
kevinkawa <kkawa@hawaii.edu>
2017-04-30 01:28:35 -1000

    addGame page to GameCollection

--- app/imports/ui/pages/addGame/addGame.js
+++ app/imports/ui/pages/addGame/addGame.js
@@ -1,48 +1,27 @@
 import { Template } from 'meteor/templating';
 import { ReactiveDict } from 'meteor/reactive-dict';
-import { _ } from 'meteor/underscore';
-import { GameTemplate, GameSchema } from '../../../api/template/template.js';
 import { FlowRouter } from 'meteor/kadira:flow-router';
-import { Meteor } from 'meteor/meteor';
-import { Games } from '/imports/api/games/GameCollection';
-import { Categories } from '/imports/api/categories/CategoryCollection';
-
-
-export const gameObjects = [{ label: 'Chess', value: '1' },
-  { label: 'Monopoly', value: '2' },
-  { label: 'Scrabble', value: '3' },
-  { label: 'Backgammon', value: '4' },
-  { label: 'Other', value: '5' }];
-
-export const maxPlayerObjects = [{ label: '2 players', value: '2' },
-  { label: '3 players', value: '3' },
-  { label: '4 players', value: '4' },
-  { label: '5 players', value: '5' },
-  { label: '6 players', value: '6' },
-  { label: '7 players', value: '7' },
-  { label: '8 players', value: '8' },
-  { label: '9 players', value: '9' },
-  { label: '10+ players', value: '10' }];
-
-export const lengthObjects = [{ label: '1 hour', value: '1' },
-  { label: '2 hours', value: '2' },
-  { label: '3 hours', value: '3' },
-  { label: '4+ hours', value: '4' }];
-
-export const smokingList = ['Allowed'];
-export const reoccurringList = ['Reoccurring'];
+import { _ } from 'meteor/underscore';
+import { Games, GamesSchema, GameTemplate } from '/imports/api/games/GameCollection';
+// import { Categories } from '/imports/api/categories/CategoryCollection';
+// import { Listings, ListingsSchema } from '/imports/api/listings/listings.js';
 
 const displayErrorMessages = 'displayErrorMessages';
+
+/* eslint-disable no-param-reassign */
 
 Template.AddGame_Page.onCreated(function onCreated() {
   // this.subscribe(Categories.getPublicationName());
   // this.subscribe(Games.getPublicationName());
   this.messageFlags = new ReactiveDict();
   this.messageFlags.set(displayErrorMessages, false);
-  this.context = GameSchema.namedContext('AddGame_Page');
+  this.context = GamesSchema.namedContext('AddGame_Page');
 });
 
 Template.AddGame_Page.helpers({
+  routeUserName() {
+    return FlowRouter.getParam('username');
+  },
   errorClass() {
     return Template.instance().messageFlags.get(displayErrorMessages) ? 'error' : '';
   },
@@ -51,62 +30,115 @@ Template.AddGame_Page.helpers({
     const errorObject = _.find(invalidKeys, (keyObj) => keyObj.name === fieldName);
     return errorObject && Template.instance().context.keyErrorMessage(errorObject.name);
   },
+  /*  categories() {
+   return _.map(categoryList, function makeCategoryObject(category) {
+   return { label: category };
+   });
+   },
+   */
+  // Not sure what this does
   game() {
     return Games.findDoc(FlowRouter.getParam('username'));
   },
-  games() {
-    return gameObjects;
-  },
-  players() {
-    return maxPlayerObjects;
-  },
-  gameLength() {
-    return lengthObjects;
-  },
-  smoking() {
-    return _.map(smokingList, function makeSmokingObject(smoking) { return { label: smoking }; });
-  },
-  reoccurring() {
-    return _.map(reoccurringList, function makeReoccurringObject(reoccurring) { return { label: reoccurring }; });
-  },
-  categories() {
-    const game = Games.findDoc(FlowRouter.getParam('username'));
-    const selectedCategories = game.categories;
-    return game && _.map(Categories.findAll(),
-            function makeInterestObject(category) {
-              return { label: category.name, selected: _.contains(selectedCategories, category.name) };
-            });
-  },
 });
 
-
 Template.AddGame_Page.events({
-  'submit .contact-data-form'(event, instance) {
+  'submit .game-data-form'(event, instance) {
     event.preventDefault();
-    // Get name (text field)
-    const gameName = event.target.GameName.value;
-    const category = event.target.Category.value;
-    const maxPlayers = event.target.MaxPlayers.value;
-    const gameLength = event.target.GameLength.value;
+    const userID = FlowRouter.getParam('username'); // schema requires username.
+    const rpgGameId = document.getElementById('gameRPG');
+    const cardGameId = document.getElementById('gameCARD');
+    const boardGameId = document.getElementById('gameBOARD');
+    const miniGameId = document.getElementById('gameMINI');
+    let category = '';
+    if (document.getElementById('RPG').checked) {
+      category = 'Role Playing';
+    } else
+      if (document.getElementById('Card').checked) {
+        category = 'Card';
+      } else
+        if (document.getElementById('Board').checked) {
+          category = 'Board';
+        } else {
+          category = 'Miniatures';
+        }
+    let gameName = '';
+    if (rpgGameId.options[rpgGameId.selectedIndex].value === '7') {
+      gameName = event.target.enterRPG.value;
+    } else
+      if (rpgGameId.options[rpgGameId.selectedIndex].value > 0) {
+        gameName = rpgGameId.options[rpgGameId.selectedIndex].text;
+      } else
+        if (cardGameId.options[cardGameId.selectedIndex].value === '7') {
+          gameName = event.target.enterCard.value;
+        } else
+          if (cardGameId.options[cardGameId.selectedIndex].value > 0) {
+            gameName = cardGameId.options[cardGameId.selectedIndex].text;
+          } else
+            if (boardGameId.options[boardGameId.selectedIndex].value === '7') {
+              gameName = event.target.enterBoard.value;
+            } else
+              if (boardGameId.options[boardGameId.selectedIndex].value > 0) {
+                gameName = boardGameId.options[boardGameId.selectedIndex].text;
+              } else
+                if (miniGameId.options[miniGameId.selectedIndex].value === '5') {
+                  gameName = event.target.enterMini.value;
+                } else
+                  if (miniGameId.options[miniGameId.selectedIndex].value > 0) {
+                    gameName = miniGameId.options[miniGameId.selectedIndex].text;
+                  } else {
+                    gameName = '';
+                  }
+    const mp = document.getElementById('Players');
+    const maxPlayers = mp.options[mp.selectedIndex].text;
+    const le = document.getElementById('Length');
+    const gameLength = le.options[le.selectedIndex].text;
     const location = event.target.Location.value;
-    const about = event.target.About.value;
-    const picture = event.target.Picture.value;
-    const contact = event.target.Contact.value;
-    const resources = event.target.Resources.value;
+    const smoking = document.getElementById('Smoking').checked;
+    const alcohol = document.getElementById('Alcohol').checked;
+    const date1 = event.target.date.value;
+    const time1 = event.target.time.value;
+    const space = ' ';
+    const date = date1 + space + time1;
+    const recurring = document.getElementById('Recurring').checked;
+    const contact = event.target.contact.value;
+    const resources = event.target.resources.value;
+    const picture = event.target.imageURL.value;
+    const about = event.target.about.value;
+    const gameID = new Date().getTime();
+    const cancelled = false;
 
-    const newGameData = { gameName, category, maxPlayers, gameLength, location, about, picture, contact, resources };
+
+    const newGameData = {
+      gameName,
+      category,
+      maxPlayers,
+      gameLength,
+      date,
+      location,
+      about,
+      smoking,
+      alcohol,
+      recurring,
+      contact,
+      resources,
+      picture,
+      userID,
+      gameID,
+      cancelled,
+    };
     // Clear out any old validation errors.
     instance.context.resetValidation();
     // Invoke clean so that newGameData reflects what will be inserted.
-   GameSchema.clean(newGameData);
+    GamesSchema.clean(newGameData);
     // Determine validity.
     instance.context.validate(newGameData);
 
     if (instance.context.isValid()) {
+      alert('Your game group has been successfully added.');
       GameTemplate.insert(newGameData);
       instance.messageFlags.set(displayErrorMessages, false);
-      const username = Meteor.user().profile.name;
-      FlowRouter.go(`/${username}/template`);
+      FlowRouter.go(FlowRouter.path('Manage_Page', FlowRouter.current().params));
     } else {
       instance.messageFlags.set(displayErrorMessages, true);
     }
