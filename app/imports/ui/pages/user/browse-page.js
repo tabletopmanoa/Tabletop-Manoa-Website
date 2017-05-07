9ac381b98befa5e98195d4115ac4f91a59dc4800
Jory Leech <joryleech@gmail.com>
2017-05-07 00:17:30 -1000

    Implemented the Manage Game Page

--- app/imports/ui/pages/user/browse-page.js
+++ app/imports/ui/pages/user/browse-page.js
@@ -1,22 +1,98 @@
 import { Template } from 'meteor/templating';
-// import { Listings } from '../../../api/listings/listings';
-import { GameTemplate } from '../../../api/games/GameCollection';
+import { Games } from '../../../api/games/GameCollection.js';
+import { ReactiveDict } from 'meteor/reactive-dict';
+import { UserToGames } from '../../../api/games/UserToGamesCollection.js';
+import { FlowRouter } from 'meteor/kadira:flow-router';
 
+Template.Browse_Page.onCreated(
+    function bodyOnCreated() {
+      this.state = new ReactiveDict();
+      this.context = Games.getSchema().namedContext('Browse_Page');
+
+      this.subscribe(Games.getPublicationName());
+      this.subscribe(UserToGames.getPublicationName());
+    }
+);
 
 Template.Browse_Page.helpers({
-
-  /**
-   * @returns {*} All of the listings documents.
-   */
-  GamesList() {
-    return GameTemplate.find();
+  gamesList() {
+    const instance = Template.instance();
+    let state = instance.state.get('category');
+    if (state === undefined) {
+      state = 'all';
+    }
+    if (state === 'all') {
+      return Games.collection().find();
+    }
+    return Games.collection().find({ category: state }, {});
+  },
+  message() {
+    return '';
+  },
+  isOpen(tID) {
+    const howMany = UserToGames.find({ ID: tID }).fetch().length;
+    if (howMany > Games.findDoc(tID).maxPlayers) {
+      return false;
+    }
+    return true;
+  },
+  alreadyPlaying(ID) {
+    const UserID = FlowRouter.getParam('username');
+    if (UserToGames.find({ ID, UserID }).fetch().length > 0) {
+      return true;
+    }
+    return false;
   },
 });
 
-// Template.Browse_Page.onCreated(function onCreated() {
-//   this.subscribe('Listings');
-// });
+Template.Browse_Page.events({
+  'change #mini-games'(event, instance) {
+    instance.state.set('mini-games', event.target.checked);
+    instance.state.set('category', 'mini');
+  },
+  'change #card-games'(event, instance) {
+    instance.state.set('card-games', event.target.checked);
+    instance.state.set('category', 'card');
+  },
+  'change #rp-games'(event, instance) {
+    instance.state.set('rp-games', event.target.checked);
+    instance.state.set('category', 'roleplaying');
+  },
+  'change #all-games'(event, instance) {
+    instance.state.set('all-games', event.target.checked);
+    instance.state.set('category', 'all');
+  },
+  'change #board-games'(event, instance) {
+    instance.state.set('board-games', event.target.checked);
+    instance.state.set('category', 'board');
+    return false;
+  },
+  'click .joinGame'(event) {
+    const ID = event.target.value;
+    const UserID = FlowRouter.getParam('username');
+    const defineObject = { ID, UserID };
+    console.log(UserToGames.find({ ID,UserID }).fetch());
+    if (UserToGames.find({ ID, UserID }).fetch().length > 0) {
+      /**
+       * This will trigger if there is a document that already exists for this user and game.
+       */
+    } else {
+      console.log(defineObject);
+      console.log(UserToGames.define(defineObject));
+      UserToGames.publish();
+    }
+    return false;
+  },
+  'click .leaveGame'(event) {
+    const ID = event.target.value;
+    const UserID = FlowRouter.getParam('username');
+    console.log(UserToGames.find({ ID, UserID }).fetch());
+    const list = UserToGames.find({ ID, UserID }).fetch();
+    for (let i = 0; i < list.length; i++) {
+      UserToGames.collection().remove(list[i]._id);
+      UserToGames.publish();
+    }
+    return false;
+  },
 
-Template.Browse_Page.onCreated(function onCreated() {
-  this.subscribe('GameTemplate');
 });
--- app/imports/ui/pages/user/manage-page.js
+++ app/imports/ui/pages/user/manage-page.js
@@ -1,12 +1,80 @@
 import { Template } from 'meteor/templating';
-// import { ReactiveDict } from 'meteor/reactive-dict';
+import { Games } from '../../../api/games/GameCollection.js';
+import { ReactiveDict } from 'meteor/reactive-dict';
+import { UserToGames } from '../../../api/games/UserToGamesCollection.js';
 import { FlowRouter } from 'meteor/kadira:flow-router';
-// import { _ } from 'meteor/underscore';
-// import { Profiles } from '/imports/api/profile/ProfileCollection';
-// import { Interests } from '/imports/api/interest/InterestCollection';
+
+Template.Manage_Page.onCreated(
+    function bodyOnCreated() {
+      this.state = new ReactiveDict();
+      this.context = Games.getSchema().namedContext('Manage_Page');
+
+      this.subscribe(Games.getPublicationName());
+      this.subscribe(UserToGames.getPublicationName());
+    }
+);
 
 Template.Manage_Page.helpers({
-  routeUserName() {
-    return FlowRouter.getParam('username');
+  runningList(){
+      console.log(Games.find().fetch());
+    const userID = FlowRouter.getParam('username');
+    return Games.find({ userID }).fetch();
   },
+  playingList() {
+    const UserID = FlowRouter.getParam('username');
+    const allMyGames = UserToGames.find({ UserID }).fetch();
+    let gameList = [];
+    for (let i = 0; i < allMyGames.length; i++) {
+      gameList = gameList.concat(Games.find({ _id: allMyGames[i].ID }).fetch());
+    }
+    return gameList;
+  },
+  message() {
+    return '';
+  },
+  isOpen(tID) {
+    const howMany = UserToGames.find({ ID: tID }).fetch().length;
+    if (howMany > Games.findDoc(tID).maxPlayers) {
+      return false;
+    }
+    return true;
+  },
+  alreadyPlaying(ID) {
+    const UserID = FlowRouter.getParam('username');
+    if (UserToGames.find({ ID, UserID }).fetch().length > 0) {
+      return true;
+    }
+    return false;
+  },
+});
+
+Template.Manage_Page.events({
+  'click .joinGame'(event) {
+    const ID = event.target.value;
+    const UserID = FlowRouter.getParam('username');
+    const defineObject = { ID, UserID };
+    console.log(UserToGames.find({ ID,UserID }).fetch());
+    if (UserToGames.find({ ID, UserID }).fetch().length > 0) {
+      /**
+       * This will trigger if there is a document that already exists for this user and game.
+       */
+    } else {
+      console.log(defineObject);
+      console.log(UserToGames.define(defineObject));
+      UserToGames.publish();
+    }
+    return false;
+  },
+  'click .leaveGame'(event) {
+    const ID = event.target.value;
+    const UserID = FlowRouter.getParam('username');
+    console.log(UserToGames.find({ ID, UserID }).fetch());
+    const list = UserToGames.find({ ID, UserID }).fetch();
+    for (let i = 0; i < list.length; i++) {
+      UserToGames.collection().remove(list[i]._id);
+      UserToGames.publish();
+    }
+    return false;
+  },
+
 });
