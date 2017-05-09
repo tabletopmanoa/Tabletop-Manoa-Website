7bdf858852cea2e319bf7afd6f3831da43c0cf9a
kodayv <kodayv@hawaii.edu>
2017-05-09 13:54:14 -1000

    added join button

--- app/imports/ui/pages/user/info-page.js
+++ app/imports/ui/pages/user/info-page.js
@@ -27,6 +27,20 @@ Template.Info_Page.helpers({
   message() {
     return '';
   },
+  isOpen(tID) {
+    const howMany = UserToGames.find({ ID: tID }).fetch().length;
+    if (howMany > Games.findDoc(tID).maxPlayers) {
+      return false;
+    }
+    return true;
+  },
+  alreadyPlaying(ID) {
+    const UserID = FlowRouter.getParam('username');
+    if (UserToGames.find({ ID, UserID }).fetch().length > 0) {
+      return true;
+    }
+    return false;
+  },
 });
 
 Template.Info_Page.events({
@@ -42,6 +56,33 @@ Template.Info_Page.events({
     } else {
       console.log(defineObject);
       console.log(UserToGames.define(defineObject));
+      UserToGames.publish();
+    }
+    return false;
+  },
+  'click .joinGame'(event) {
+    const ID = event.target.value;
+    const UserID = FlowRouter.getParam('username');
+    const defineObject = { ID, UserID };
+    console.log(UserToGames.find({ ID, UserID }).fetch());
+    if (UserToGames.find({ ID, UserID }).fetch().length > 0) {
+      /**
+       * This will trigger if there is a document that already exists for this user and game.
+       */
+    } else {
+      console.log(defineObject);
+      console.log(UserToGames.define(defineObject));
+      UserToGames.publish();
+    }
+    return false;
+  },
+  'click .leaveGame'(event) {
+    const ID = event.target.value;
+    const UserID = FlowRouter.getParam('username');
+    console.log(UserToGames.find({ ID, UserID }).fetch());
+    const list = UserToGames.find({ ID, UserID }).fetch();
+    for (let i = 0; i < list.length; i++) {
+      UserToGames.collection().remove(list[i]._id);
       UserToGames.publish();
     }
     return false;
